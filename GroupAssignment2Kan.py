0b25f3ae7db63c1719db233c6820badb5006b436
vlk3 <vlk3@duke.edu>
2015-02-27 13:20:00 -0500

    Finished monthly aggregation; all the codes ran fine on my computer. Someone with a non-donkey computer please run the codes with the full dataset. Feel free to clean up the codes.

--- GroupAssignment2Kan.py
+++ GroupAssignment2Kan.py
@@ -26,7 +26,7 @@ df2 = df2[(df2['RES_Tariff'] == 'A') & (df2['RES_Stimulus']== '1') | (df2['RES_S
 df = pd.merge(df, df2, on = 'ID')
 # Can use df.sort[('')] to check to make sure all the tariff and stimulus groups are represented. I'm sure there's an easier way... oh well
 
-#df1 = pd.concat(df, ignore_index = True)
+## Splitting the DAYHH column into Day and HH
 df['hour_cer'] = df['DAYHH'] %100
 df['day_cer'] = (df['DAYHH']-df['hour_cer']% 100)/100
 df = df[['ID', 'DAYHH', 'hour_cer', 'day_cer', 'kwh', 'Code', 'RES_Tariff', 'RES_Stimulus']]
@@ -73,41 +73,40 @@ ax2 = fig1.add_subplot(2,1,2) # two rows, one column, first plot
 ax2.plot(t_p['day_cer'], t_p['pval'])
 ax2.axhline(0.05, color='r', linestyle='--')
 ax2.axvline(14, color='g', linestyle='--')
-ax2.set_title('p-values over-time')
+ax2.set_title('p-values over-time (montly)')
 plt.show()
 
 # MONTHLY AGGREGATION 
-grp = df3.groupby(['ID','year','month', 'RES_Stimulus']) ## Dan said to group by year and month
-agg = grp['kwh'].sum()
-grp.sum() 
+grp2 = df3.groupby(['ID','year','month','RES_Stimulus']) ## Dan said to group by year and month
+agg1 = grp2['kwh'].sum()
 
 # reset the index (multilevel at the moment)
-agg = agg.reset_index() # drop the multi-index
-grp1 = agg.groupby(['day_cer','RES_Stimulus']) 
+agg1 = agg1.reset_index() # drop the multi-index
+grp3 = agg1.groupby(['year', 'month', 'RES_Stimulus']) 
 #agg.head() to look at first five rows
 
 ## split up treatment/control
-trt = {(k[0]): agg.kwh[v].values for k, v in grp1.groups.iteritems() if k[1] == '1'} # get set of all treatments by date
-ctrl = {(k[0]): agg.kwh[v].values for k, v in grp1.groups.iteritems() if k[1] == 'E'} # get set of all controls by date
-keys = ctrl.keys()
+trt1 = {(k[0], k[1]): agg1.kwh[v].values for k, v in grp3.groups.iteritems() if k[2] == '1'} # get set of all treatments by year-month
+ctrl1 = {(k[0], k[1]): agg1.kwh[v].values for k, v in grp3.groups.iteritems() if k[2] == 'E'} # get set of all controls by year-month
+keys2 = ctrl1.keys()
 
 # tstats and pvals
-tstats = DataFrame([(k, np.abs(float(ttest_ind(trt[k], ctrl[k], equal_var=False)[0]))) for k in keys], columns=['day_cer', 'tstat'])
-pvals = DataFrame([(k, (ttest_ind(trt[k], ctrl[k], equal_var=False)[1])) for k in keys], columns=['day_cer', 'pval'])
-t_p = pd.merge(tstats, pvals)
+tstats1 = DataFrame([(k, np.abs(float(ttest_ind(trt1[k], ctrl1[k], equal_var=False)[0]))) for k in keys2], columns=['month', 'tstat1'])
+pvals1 = DataFrame([(k, (ttest_ind(trt1[k], ctrl1[k], equal_var=False)[1])) for k in keys2], columns=['month', 'pval1'])
+t_p1 = pd.merge(tstats1, pvals1)
 
 
 #Plotting!
-fig1 = plt.figure() # initialize plot
-ax1 = fig1.add_subplot(2,1,1) # two rows, one column, first plot
-ax1.plot(t_p['day_cer'],t_p['tstat'])
-ax1.axhline(2, color='r', linestyle='--')
-ax1.axvline(14, color='g', linestyle='--')
-ax1.set_title('t-stats over-time (daily)')
+fig2 = plt.figure() # initialize plot
+ax3 = fig2.add_subplot(2,1,1) # two rows, one column, first plot
+ax3.plot(t_p1['tstat1'])
+ax3.axhline(2, color='r', linestyle='--')
+ax3.axvline(14, color='g', linestyle='--')
+ax3.set_title('t-stats over-time (monthly)')
 
-ax2 = fig1.add_subplot(2,1,2) # two rows, one column, first plot
-ax2.plot(t_p['day_cer'], t_p['pval'])
-ax2.axhline(0.05, color='r', linestyle='--')
-ax2.axvline(14, color='g', linestyle='--')
-ax2.set_title('p-values over-time')
+ax4 = fig2.add_subplot(2,1,2) # two rows, one column, first plot
+ax4.plot(t_p1['pval1'])
+ax4.axhline(0.05, color='r', linestyle='--')
+ax4.axvline(14, color='g', linestyle='--')
+ax4.set_title('p-values over-time (monthly')
 plt.show()
